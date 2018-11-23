51ba9145b380db4fd216c5560b6bcad6c08d9819
304978499@qq.com <304978499@qq.com>
2018-11-23 11:25:03 +0800

    服务可以注册进入eureka,info可以用,消费者可正常访问

--- panda-provider-8001/src/main/java/com/fhz/panda/PandaProvider8001_StartApp.java
+++ panda-provider-8001/src/main/java/com/fhz/panda/PandaProvider8001_StartApp.java
@@ -2,8 +2,14 @@ package com.fhz.panda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableScheduling
 public class PandaProvider8001_StartApp {
 
 	public static void main(String[] args) {
